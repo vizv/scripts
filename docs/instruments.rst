b796f8e2bb667bd73fe5c18fd498772158c6fb9d
Atte Haarni <atte.h.1998@gmail.com>
2024-04-27 19:07:25 +0300

    Add instruments order
    Add a subcommand to instruments for creating instrument work orders

--- docs/instruments.rst
+++ docs/instruments.rst
@@ -2,19 +2,37 @@ instruments
 ===========
 
 .. dfhack-tool::
-    :summary: Show how to craft instruments.
-    :tags: fort inspection
+    :summary: Show how to craft instruments, create instrument work orders.
+    :tags: fort inspection workorders
 
-This tool provides information on how to craft the instruments used by the
+
+This tool is used to query information about instruments, and create work orders for them.
+
+The list subcommand provides information on how to craft the instruments used by the
 player civilization. For single-piece instruments, it shows the skill and
 material needed to craft it. For multi-piece instruments, it displays the skill
 used in its assembly as well as information on how to craft the necessary
 pieces. It also shows whether the instrument is handheld or placed as a
 building.
 
+The order subcommand is used to create work orders for an instrument and all of it's parts.
+The final assemble instrument -order waits for the part orders to complete before starting.
+
 Usage
 -----
 
-::
+``instruments``, ``instruments list``
+List instruments and their recipes
 
-    instruments
+``instruments order <instrument_name> [<amount>]``
+Creates work orders for the specified instrument. Default amount is 1.
+
+Examples
+--------
+
+``instruments order givel 10``
+Creates a total of 5 work orders. One for assembling 10 givels, and one to create 10 of
+each of the givel parts. (Note: Instruments are randomly generated, so you probably won't have the same givel in your game)
+
+``instruments order ilul``
+Creates work orders to assemble one ïlul. Spelling doesn't need to include the special ï character.
--- instruments.lua
+++ instruments.lua
@@ -1,13 +1,16 @@
+local workorder = reqscript('workorder')
+
 -- civilization ID of the player civilization
 local civ_id = df.global.plotinfo.civ_id
+local raws = df.global.world.raws
 
 ---@type instrument itemdef_instrumentst
 ---@return reaction|nil
-function getAssemblyReaction(instrument)
-    for _, reaction in ipairs(df.global.world.raws.reactions.reactions) do
+function getAssemblyReaction(instrument_id)
+    for _, reaction in ipairs(raws.reactions.reactions) do
         if reaction.source_enid == civ_id and
             reaction.category == 'INSTRUMENT' and
-            reaction.name:find(instrument.name, 1, true)
+            reaction.code:find(instrument_id, 1, true)
         then
             return reaction
         end
@@ -23,7 +26,7 @@ function reagentString(reagent)
         local silk = reagent.flags2.silk and "silk " or ""
         local yarn = reagent.flags2.yarn and "yarn " or ""
         local plant = reagent.flags2.plant and "plant " or ""
-        return  silk..yarn..plant.."thread"
+        return silk .. yarn .. plant .. "thread"
     else
         return reagent.code
     end
@@ -40,31 +43,107 @@ function describeReaction(reaction)
     return skill .. ": " .. table.concat(reagents, ", ")
 end
 
--- gather instrument piece reactions and index them by the instrument they are part of
-local instruments = {}
-for _, reaction in ipairs(df.global.world.raws.reactions.reactions) do
-    if reaction.source_enid == civ_id and reaction.category == 'INSTRUMENT_PIECE' then
-        local iname = reaction.name:match("[^ ]+ ([^ ]+)")
-        table.insert(ensure_key(instruments, iname),
-                     reaction.name.." ("..describeReaction(reaction)..")")
+local function print_list()
+    -- gather instrument piece reactions and index them by the instrument they are part of
+    local instruments = {}
+    for _, reaction in ipairs(raws.reactions.reactions) do
+        if reaction.source_enid == civ_id and reaction.category == 'INSTRUMENT_PIECE' then
+            local iname = reaction.name:match("[^ ]+ ([^ ]+)")
+            table.insert(ensure_key(instruments, iname),
+                reaction.name .. " (" .. describeReaction(reaction) .. ")")
+        end
+    end
+
+    -- go over instruments
+    for _, instrument in ipairs(raws.itemdefs.instruments) do
+        if not (instrument.source_enid == civ_id) then goto continue end
+
+        local building_tag = instrument.flags.PLACED_AS_BUILDING and " (building, " or " (handheld, "
+        local reaction = getAssemblyReaction(instrument.id)
+        dfhack.print(instrument.name .. building_tag)
+        if #instrument.pieces == 0 then
+            print(describeReaction(reaction) .. ")")
+        else
+            print(df.job_skill[reaction.skill] .. "/assemble)")
+            for _, str in pairs(instruments[instrument.name]) do
+                print("  " .. str)
+            end
+        end
+        print()
+        ::continue::
     end
 end
 
--- go over instruments
-for _,instrument in ipairs(df.global.world.raws.itemdefs.instruments) do
-    if not (instrument.source_enid == civ_id) then goto continue end
+local function order_instrument(name, amount)
+    local instrument = nil
 
-    local building_tag = instrument.flags.PLACED_AS_BUILDING and " (building, " or " (handheld, "
-    local reaction = getAssemblyReaction(instrument)
-    dfhack.print(instrument.name..building_tag)
-    if #instrument.pieces == 0 then
-        print(describeReaction(reaction)..")")
-    else
-        print(df.job_skill[reaction.skill].."/assemble)")
-        for _,str in pairs(instruments[instrument.name]) do
-            print("  "..str)
+    for _, instr in ipairs(raws.itemdefs.instruments) do
+        if dfhack.toSearchNormalized(instr.name) == name and instr.source_enid == civ_id then
+            instrument = instr
         end
     end
-    print()
-    ::continue::
+
+    if instrument == nil then
+        qerror("Could not find instrument " .. name)
+    end
+
+    local orders = {}
+
+    for i, reaction in ipairs(raws.reactions.reactions) do
+        if reaction.source_enid == civ_id and reaction.category == 'INSTRUMENT_PIECE' and reaction.code:find(instrument.id, 1, true) then
+            local part_order = {
+                id=i,
+                amount_total=amount,
+                reaction=reaction.code,
+                job="CustomReaction",
+            }
+            table.insert(orders, part_order)
+        end
+    end
+
+    if #orders < #instrument.pieces then
+        print("Warning: Could not find reactions for all instrument pieces")
+    end
+
+    local assembly_reaction = getAssemblyReaction(instrument.id)
+
+    local assembly_order = {
+        id=-1,
+        amount_total=amount,
+        reaction=assembly_reaction.code,
+        job="CustomReaction",
+        order_conditions={}
+    }
+
+    for _, order in ipairs(orders) do
+        table.insert(
+            assembly_order.order_conditions,
+            {
+                condition="Completed",
+                order=order.id
+            }
+        )
+    end
+
+    table.insert(orders, assembly_order)
+
+    orders = workorder.preprocess_orders(orders)
+    workorder.fillin_defaults(orders)
+    workorder.create_orders(orders)
+
+    print("\nCreated " .. #orders .. " work orders")
+end
+
+local args = { ... }
+
+if #args == 0 or args[1] == "list" then
+    print_list()
+elseif args[1] == "order" then
+    local instrument_name = args[2]
+    if instrument_name == nil then
+        qerror("Usage: instruments order <instrument_name> [<amount>]")
+    end
+
+    local amount = args[3] or 1
+    order_instrument(instrument_name, amount)
 end
