bfcf9c3fca21974f27c4f3eb9dbc1388c66ebfa1
Alex Noir <Varsash@gmail.com>
2024-05-29 03:43:32 +0300

    Overhaul and update the script for the Steam release

--- adv-rumors.lua
+++ adv-rumors.lua
@@ -1,99 +1,114 @@
 -- Improve "Bring up specific incident or rumor", "Ask for Directions" and "Ask about Somebody" menus in Adventure mode
---@ enable = true
---[====[
+
+--@ module=true
+
+local help = [====[
 
 adv-rumors
 ==========
 Improves the "Bring up specific incident or rumor" menu in Adventure mode.
 
-- Moves entries into one line
 - Adds a "slew" keyword for filtering, making it easy to find your kills and not your companions'
 - Adds "gaining", "placement" and "giving" keywords for artifacts
 - Trims repetitive words
 
 ]====]
 
---========================
--- Author : 1337G4mer on bay12 and reddit, version 0.3 by Crystalwarrior on Discord
--- Version : 0.3
--- Description : An utility based on dfhack to improve the rumor, directions and ask about... UIs in adventure mode.
---
---      "Bring up specific incident or rumor", "Ask for Directions" and "Ask about Somebody" menus will
---      be automatically improved with better searchability and organization.
---
--- Prior Configuration: (you can skip this if you want)
---      Set the three boolean values below and play around with the script as to how you like
---      improveReadability = will move everything in one line
---      addPrefix = will add prefixes for every supported type of incident (Fight: , Event: , Rumor: , Place: , Person: )
---      addKeywords = will add a lot more keywords to make the rumors more searchable, such as missing names, "slew" keyword, "me" where "you" is used, etc.
---      shortenString = will further shorten the line to = slew "XYZ" ( "n time" ago in " Region")
---=======================
-local improveReadability = true
-local addPrefix = true
-local addKeywords = true
-local shortenString = true
+local adventure = df.global.game.main_interface.adventure
 
-local utils = require "utils"
+local utils = require("utils")
 
-function condenseChoiceTitle(choice)
-    while #choice.title > 1 do
-        choice.title[0].value = choice.title[0].value .. choice.title[1].value
-        choice.title:erase(1)
+local overlay = require('plugins.overlay')
+local widgets = require('gui.widgets')
+
+-- experimental, set this to 'true' to make the choices less verbose
+local shortening = false
+
+AdvRumorsOverlay = defclass(AdvRumorsOverlay, overlay.OverlayWidget)
+AdvRumorsOverlay.ATTRS{
+    desc='Sample widget that displays a message on the screen.',
+    default_pos={x=25,y=25},
+    default_enabled=true,
+    viewscreens='dungeonmode/Conversation',
+    overlay_onupdate_max_freq_seconds=0,
+}
+
+function AdvRumorsOverlay:init()
+    self:addviews{
+        widgets.Label{
+            view_id='label',
+            text='adv-rumors - waiting...',
+            auto_width=true
+        },
+    }
+end
+
+function AdvRumorsOverlay:overlay_onupdate()
+    checkRumorUpdate()
+end
+
+OVERLAY_WIDGETS = {message=AdvRumorsOverlay}
+
+-- CORE FUNCTIONS
+
+-- Converts the choice's print string to a lua string
+function choiceToString(choice)
+    local line_table = {}
+    for i, data in ipairs(choice.print_string.text) do
+        table.insert(line_table, data.value)
+    end
+    return table.concat(line_table, "\n")
+end
+
+-- Renames the choice's print string based on string, respecting the newlines
+function renameChoice(text, choice)
+    -- Clear the string
+    for i, data in ipairs(choice.print_string.text) do
+        df.delete(data)
+    end
+    choice.print_string.text:resize(0)
+
+    -- Split the string assuming \n is newline
+    local line_table = string.split(text, "\n")
+    for i, line in ipairs(line_table) do
+        -- Create a df string for each line
+        local line_ptr = df.new('string')
+        line_ptr.value = line
+        -- Insert it into the text data
+        choice.print_string.text:insert('#', line_ptr)
     end
 end
 
-function addPrefixByType(choice)
-    if choice.choice.type == df.talk_choice_type.SummarizeConflict and not string.find(choice.title[0].value, "^Fight: ") then
-        choice.title[0].value = "Fight: " .. choice.title[0].value
+function addKeyword(choice, keyword)
+    -- Prevent duplicate keywords
+    for i, kword in ipairs(choice.key_word) do
+        if kword.value == keyword then
+            return
+        end
     end
-    if choice.choice.type == df.talk_choice_type.BringUpEvent and not string.find(choice.title[0].value, "^Event: ") then
-        choice.title[0].value = "Event: " .. choice.title[0].value
-    end
-    if choice.choice.type == df.talk_choice_type.SpreadRumor and not string.find(choice.title[0].value, "^Rumor: ") then
-        choice.title[0].value = "Rumor: " .. choice.title[0].value
-    end
-    if choice.choice.type == df.talk_choice_type.AskDirectionsPlace and not string.find(choice.title[0].value, "^Place: ") then
-        choice.title[0].value = "Place: " .. choice.title[0].value
-    end
-    if choice.choice.type == df.talk_choice_type.AskAboutPerson and not string.find(choice.title[0].value, "^Whom: ") then
-        choice.title[0].value = "Whom: " .. choice.title[0].value
-    end
-    if choice.choice.type == df.talk_choice_type.AskWhereabouts and not string.find(choice.title[0].value, "^Who: ") then
-        choice.title[0].value = "Who: " .. choice.title[0].value
-    end
+    local keyword_ptr = df.new('string')
+    keyword_ptr.value = keyword
+    choice.key_word:insert('#', keyword_ptr)
 end
 
-function addKeywordsForChoice(choice)    
-    if string.find(choice.title[0].value, "slew") then
+function addKeywordsForChoice(choice)
+    local fulltext = choiceToString(choice)
+
+    -- Special cases
+    if string.find(fulltext, "slew") or string.find(fulltext, "slain") then
         addKeyword(choice, 'slew')
+        addKeyword(choice, 'slay')
+        addKeyword(choice, 'slain')
     end
-    if string.find(choice.title[0].value, "attack") then
-        addKeyword(choice, 'attack')
-    end
-    if string.find(choice.title[0].value, " you ") or string.find(choice.title[0].value, " your ") then
+
+    -- add a "sane" handling of you/your/me
+    if string.find(fulltext, " you ") or string.find(fulltext, " your ") then
         addKeyword(choice, 'me')
-    end
-    if choice.choice.type == df.talk_choice_type.SummarizeConflict then
-        addKeyword(choice, "conflict") -- keyword 'conflict' already exists
-    end
-    if choice.choice.type == df.talk_choice_type.BringUpEvent then
-        addKeyword(choice, "event")
-    end
-    if choice.choice.type == df.talk_choice_type.SpreadRumor then
-        addKeyword(choice, "rumor")
-    end
-    if choice.choice.type == df.talk_choice_type.AskDirectionsPlace then
-        addKeyword(choice, "directions")
-        addKeyword(choice, "place")
-    end
-    if choice.choice.type == df.talk_choice_type.AskWhereabouts then
-        addKeyword(choice, "person")
-        addKeyword(choice, "whereabouts")
     end
 
     -- Transform the whole thing into keywords barring blacklist
-    local names_blacklist = utils.invert{"the", "a", "an", "you", "your", "of", "to", "attacked", "slew", "was", "slain", "by"}
-    local title = choice.title[0].value
+    local names_blacklist = utils.invert{"the", "a", "an", "of", "to", "attacked", "slew", "was", "slain", "by"}
+    local title = fulltext
     if title:find('%(') then
         title = title:sub(1, title:find('%(') - 1)
     end
@@ -106,9 +121,10 @@ function addKeywordsForChoice(choice)
     end
 end
 
-function shortenChoice(choice)
-    choice.title[0].value = choice.title[0].value
-        :gsub("Summarize the conflict in which +", "")
+-- Returns a string that shortens the options
+function shortenChoiceText(text)
+    return text
+        :gsub("Summarize the conflict in which +", "A fight where ")
         :gsub("This occurred +", "")
         :gsub("Bring up +", "")
         :gsub("Spread rumor of +", "")
@@ -117,112 +133,31 @@ function shortenChoice(choice)
         :gsub("Ask for the whereabouts of +", "where is ")
 end
 
-function addKeyword(choice, keyword)
-    -- Prevent duplicate keywords
-    for i, kword in ipairs(choice.keywords) do
-        if kword.value == keyword then
-            return
-        end
-    end
-    local keyword_ptr = df.new('string')
-    keyword_ptr.value = keyword
-    choice.keywords:insert('#', keyword_ptr)
+function shortenChoice(choice)
+    local fulltext = choiceToString(choice)
+    fulltext = shortenChoiceText(fulltext)
+    renameChoice(fulltext, choice)
 end
 
--- Helper function to create new dialog choices
-function new_choice(choice_type, title, keywords)
-    local dialog = df.global.ui_advmode.conversation
-    dialog.choices:insert("#", {new = df.ui_advmode.T_conversation.T_choices,})
-    local choice_idx = #dialog.choices-1
-    local choice = dialog.choices[choice_idx]
-    if choice.choice == nil then
-        choice.choice = df.talk_choice:new()
+-- Check if rumorUpdate() should be called, making sure to keep track of the current conversation tree state
+local current_state = -1
+function checkRumorUpdate()
+    if not adventure.conversation.open or adventure.conversation.selecting_conversation or adventure.conversation.conv_actce == nil then
+        current_state = -1
+        return
     end
-    choice.choice.type = choice_type
-    choice.title:insert("#",df.new("string"))
-    choice.title[0].value=title
-    for i, key in ipairs(keywords) do
-        addKeyword(choice, key)
+    if current_state ~= adventure.conversation.conv_actce.state then
+        rumorUpdate()
     end
-
-    dialog.page_bottom_choices[0] = choice_idx
+    current_state = adventure.conversation.conv_actce.state
 end
 
 -- Condense the rumor system choices
 function rumorUpdate()
-    for i, choice in ipairs(df.global.ui_advmode.conversation.choices) do
-        if choice.choice.type == df.talk_choice_type.SummarizeConflict or
-           choice.choice.type == df.talk_choice_type.BringUpEvent or
-           choice.choice.type == df.talk_choice_type.SpreadRumor or
-           choice.choice.type == df.talk_choice_type.AskAboutPerson or 
-           choice.choice.type == df.talk_choice_type.AskDirectionsPlace or
-           choice.choice.type == df.talk_choice_type.AskWhereabouts then
-            if improveReadability then
-                condenseChoiceTitle(choice)
-            end
-            if addPrefix then
-                addPrefixByType(choice)
-            end
-            if shortenString then
-                condenseChoiceTitle(choice)
-                shortenChoice(choice)
-            end
-            if addKeywords then
-                addKeywordsForChoice(choice)
-            end
+    for i, choice in ipairs(adventure.conversation.conv_choice_info) do
+        if shortening and adventure.conversation.conv_actce.state ~= df.conversation_state_type.MAIN then
+            shortenChoice(choice)
         end
-    end
-end
-
--- Optionally add new choices in addition of existing ones
-function choiceUpdate()
-    if df.global.ui_advmode.conversation.activity_event[0].menu == df.conversation_menu.MainMenu then
-        -- Essentially the "weather talking" exploit (as described by Rumrusher) in a single speaking action, put this in more menus to see its awesome potential.
-        new_choice(df.talk_choice_type.AskTargetAction, "Ask what will they do about it", {"initiative", "action", "speak", "opinion"})
-    end
-end
-
--- Main Loop
-active = active or false
-function rumorloop()
-    if active then dfhack.timeout(1, 'frames', check) end
-end
-
--- Check if Continue Looping
-local last_menu = nil
-function check()
-    if not dfhack.world.isAdventureMode() then
-        return
-    end
-    if df.global.ui_advmode.menu ~= df.ui_advmode_menu.ConversationSpeak then
-        last_menu = nil
-        rumorloop()
-        return
-    end
-    if df.global.ui_advmode.conversation.activity_event[0].menu ~= last_menu then
-        rumorUpdate()
-        -- Experimental "add extra choices" system, disabled by default. Uncomment the line below to test it out!
-        -- choiceUpdate()
-        last_menu = df.global.ui_advmode.conversation.activity_event[0].menu
-    end
-    rumorloop()
-end
-
--- onStateChange listener to start/stop looping in relevant context
-function dfhack.onStateChange.advRumorConversation (code)
-    if code == SC_VIEWSCREEN_CHANGED and dfhack.isWorldLoaded() then
-        local scr = dfhack.gui.getCurViewscreen()
-        set_listener_active(scr._type == df.viewscreen_dungeonmodest)
-    end
-end
-
--- Toggle Loop on/off
-function set_listener_active(tog)
-    active = tog
-    if active then
-        print("activating rumor listener")
-        check()
-    else
-        print("de-activating rumor listener")
+        addKeywordsForChoice(choice)
     end
 end
