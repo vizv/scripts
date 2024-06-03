6c5fa00899bd2cae1436018e74205a35ee5e798f
Alex Noir <Varsash@gmail.com>
2024-06-03 05:04:44 +0300

    Fix companion pets not being associated with your adventuring party (extra companions will not have their pets shown in the Companion screen but they'll still fast travel alongside you)

--- fix/gifted-pets.lua
+++ fix/gifted-pets.lua
@@ -1,43 +1,112 @@
 -- Fixes pets gifted to another unit in Adventure Mode
+-- Fixes pets owned by companions missing from the companions list
 
-local adventurer_record = df.nemesis_record.find(df.global.adventure.player_id)
-local alreadyProcessed = {}
-
-for i, nem_id in ipairs(adventurer_record.companions) do
-    local companion_record = df.nemesis_record.find(nem_id)
-    if companion_record.unit then
-        local pet_owner = companion_record.unit.relationship_ids.Pet
-        if pet_owner ~= -1 and pet_owner ~= adventurer_record.unit_id then
-            local pet_owner_record = df.nemesis_record.find(pet_owner)
-            -- pet_owner_record.histfig_links
-            local site_links = pet_owner_record.figure.site_links
-
-            -- If the person we're gifting the pet to is part of an active army
-            if pet_owner_record.unit.enemy.army_controller_id ~= -1 and pet_owner_record.unit.enemy.army_controller then
-                companion_record.unit.army_controller_id = pet_owner_record.unit.enemy.army_controller_id
-                companion_record.unit.army_controller = pet_owner_record.unit.enemy.army_controller
+local function isPetOf(petNemesis, ownerNemesis)
+    if petNemesis.unit then
+        local owner_id = petNemesis.unit.relationship_ids.Pet
+        if owner_id ~= -1 and owner_id == ownerNemesis.unit_id then
+            return true
+        end
+    elseif petNemesis.figure then -- in case the unit is offloaded
+        for _, link in ipairs(petNemesis.figure.histfig_links) do
+            if link._type == df.histfig_hf_link_pet_ownerst and ownerNemesis.figure and link.target_hf == ownerNemesis.figure.id then
+                return true
             end
+        end
+    end
+    return false
+end
 
-            companion_record.unit.relationship_ids.GroupLeader = pet_owner_record.unit_id
-            companion_record.group_leader_id = pet_owner
-            
-            for u, link in ipairs(site_links) do
-                local new_site_link = df.new(link)
-                new_site_link.site = link.site
-                new_site_link.sub_id = link.sub_id
-                new_site_link.entity = link.entity
 
-                companion_record.figure.site_links:insert('#', new_site_link)
+local function findPetOwnerOf(petNemesis)
+    if petNemesis.figure then
+        for _, link in ipairs(petNemesis.figure.histfig_links) do
+            if link._type == df.histfig_hf_link_pet_ownerst then
+                local target_hf = link.target_hf
+                return df.nemesis_record.find(df.historical_figure.find(target_hf).nemesis_id)
             end
-            -- pet_owner_record.companions:insert('#', companion_record.id)
-            alreadyProcessed[tostring(companion_record.id)] = true
+        end
+    end
+    return nil
+end
+
+local function findInParty(party, histfig_id)
+    for k, v in ipairs(party) do
+        if v == histfig_id then
+            return true
+        end
+    end
+    return false
+end
+
+local function fixMissingCompanionPets(nemesis)
+    local party = df.global.adventure.interactions
+    for i, nem_id in ipairs(nemesis.companions) do
+        local pet_record = df.nemesis_record.find(nem_id)
+        if isPetOf(pet_record, nemesis) and not findInParty(party.party_pets, pet_record.figure.id) then
+            print("inserting pet "..pet_record.id.." of companion "..nemesis.id.." into the party!")
+            party.party_pets:insert('#', pet_record.figure.id)
         end
     end
 end
 
--- clean up our records
-for index = #adventurer_record.companions-1, 0, -1 do
-    if alreadyProcessed[tostring(adventurer_record.companions[index])] then
-        adventurer_record.companions:erase(index)
+
+local function fixGiftedPet(pet_record, pet_owner_record)
+    -- associate the unit relationships
+    if pet_record.unit then
+        pet_record.unit.relationship_ids.Pet = pet_owner_record.unit_id
+        pet_record.unit.relationship_ids.GroupLeader = pet_owner_record.unit_id
+        pet_record.group_leader_id = pet_owner_record.id
+    end
+    
+    -- associate the site links
+    local site_links = pet_owner_record.figure.site_links
+    for u, link in ipairs(site_links) do
+        if link._type == df.histfig_site_link_home_site_abstract_buildingst or
+        link._type == df.histfig_site_link_home_site_realization_buildingst or
+        link._type == df.histfig_site_link_lairst or
+        link._type == df.histfig_site_link_home_site_realization_sulst or
+        link._type == df.histfig_site_link_home_site_saved_civzones then
+            -- Make a copy of the link
+            local new_site_link = df.new(link)
+            -- Assign the same data
+            new_site_link.site = link.site
+            new_site_link.sub_id = link.sub_id
+            new_site_link.entity = link.entity
+            -- Insert that data into the pet's historical figure data
+            pet_record.figure.site_links:insert('#', new_site_link)
+        end
     end
 end
+
+local function fixGiftedPets(nemesis)
+    local queueCompanionErase = {}
+    local party = df.global.adventure.interactions
+    for i, nem_id in ipairs(nemesis.companions) do
+        local companion_record = df.nemesis_record.find(nem_id)
+        fixMissingCompanionPets(companion_record)
+
+        if not findInParty(party.party_pets, companion_record.figure.id) then
+            print("nemesis "..nem_id.." is present in nemesis record but not part of the adventurer's party, checking if it's a pet")
+            local pet_owner_record = findPetOwnerOf(companion_record)
+            if pet_owner_record then
+                print("pet owner confirmed! Identified nemesis "..pet_owner_record.id.." as owner.")
+                local party = party
+                local owner_in_party = findInParty(party.party_core_members, companion_record.figure.id) or findInParty(party.party_extra_members, companion_record.figure.id)
+                if not owner_in_party then
+                    print("the pet's owner is NOT a companion. We need to associate the pet with the owner correctly so the pet's unit is not inaccessible.")
+                    fixGiftedPet(companion_record, pet_owner_record)
+                    queueCompanionErase[tostring(pet_record.id)] = true
+                end
+            end
+        end
+    end
+    for index = #nemesis.companions-1, 0, -1 do
+        if queueCompanionErase[tostring(nemesis.companions[index])] then
+            print("erasing bugged pet "..nemesis.companions[index].." from "..nemesis.id.."'s companions list")
+            nemesis.companions:erase(index)
+        end
+    end
+end
+
+fixGiftedPets(df.nemesis_record.find(df.global.adventure.player_id))
