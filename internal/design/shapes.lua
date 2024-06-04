4100d8211453162db562726d5f25b3961b9eac96
Myk Taylor <myk002@yahoo.com>
2024-06-04 01:37:30 -0700

    take bezier points into account for granularity

--- internal/design/shapes.lua
+++ internal/design/shapes.lua
@@ -470,16 +470,19 @@ function Line:plot_bresenham(x0, y0, x1, y1, thickness)
 
 end
 
-local function get_granularity(x0, y0, x1, y1)
-    local line_len = math.abs(x1 - x0) + math.abs(y1 - y0)
-    return 1 / (line_len * 10)
+local function get_granularity(x0, y0, x1, y1, bezier_point1, bezier_point2)
+    local spread_x = math.max(x0, x1,  bezier_point1.x, bezier_point2 and bezier_point2.x or 0) -
+        math.min(x0, x1,  bezier_point1.x, bezier_point2 and bezier_point2.x or math.huge)
+    local spread_y = math.max(y0, y1,  bezier_point1.y, bezier_point2 and bezier_point2.y or 0) -
+        math.min(y0, y1,  bezier_point1.y, bezier_point2 and bezier_point2.y or math.huge)
+    return 1 / ((spread_x + spread_y) * 10)
 end
 
 function Line:cubic_bezier(x0, y0, x1, y1, bezier_point1, bezier_point2, thickness)
     local t = 0
     local x2, y2 = bezier_point1.x, bezier_point1.y
     local x3, y3 = bezier_point2.x, bezier_point2.y
-    local granularity = get_granularity(x0, y0, x1, y1)
+    local granularity = get_granularity(x0, y0, x1, y1, bezier_point1, bezier_point2)
     while t <= 1 do
         local x = math.floor(((1 - t) ^ 3 * x0 + 3 * (1 - t) ^ 2 * t * x2 + 3 * (1 - t) * t ^ 2 * x3 + t ^ 3 * x1) +
             0.5)
@@ -516,7 +519,7 @@ end
 function Line:quadratic_bezier(x0, y0, x1, y1, bezier_point1, thickness)
     local t = 0
     local x2, y2 = bezier_point1.x, bezier_point1.y
-    local granularity = get_granularity(x0, y0, x1, y1)
+    local granularity = get_granularity(x0, y0, x1, y1, bezier_point1)
     while t <= 1 do
         local x = math.floor(((1 - t) ^ 2 * x0 + 2 * (1 - t) * t * x2 + t ^ 2 * x1) + 0.5)
         local y = math.floor(((1 - t) ^ 2 * y0 + 2 * (1 - t) * t * y2 + t ^ 2 * y1) + 0.5)
