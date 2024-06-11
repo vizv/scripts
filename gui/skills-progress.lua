81ad2bdb76b89363af9650ecb3dde72428bbd247
Alex Noir <Varsash@gmail.com>
2024-06-11 17:11:04 +0300

    Add hotkey labels and ability to toggle the progress bar and experience display Adjust the frame stuff

--- gui/skills-progress.lua
+++ gui/skills-progress.lua
@@ -44,24 +44,59 @@ SkillProgressOverlay.ATTRS {
         'dungeonmode/ViewSheets/UNIT/Skills/Social',
         'dungeonmode/ViewSheets/UNIT/Skills/Other',
     },
-    frame={w=16, h=66},
+    frame={w=54, h=67},
 }
 
 function SkillProgressOverlay:init()
-    self.dirty = true
+    self.progress_bar = true
+    self.display_experience = true
 
     self:addviews{
         widgets.Label{
             view_id='annotations',
-            frame={t=0, l=0},
+            frame={t=0, l=self.frame.w-16, w=16},
             text='',
             text_pen=COLOR_GRAY,
-        }
+        },
+        widgets.ToggleHotkeyLabel{
+            frame={t=self.frame.h-1,l=2, w=25},
+            label='Progress Bar',
+            key='CUSTOM_CTRL_B',
+            options={
+                {label='No', value=false, pen=COLOR_WHITE},
+                {label='Yes', value=true, pen=COLOR_YELLOW},
+            },
+            view_id='toggle_progress',
+            on_change=function(option) self.progress_bar = option end,
+        },
+        widgets.ToggleHotkeyLabel{
+            frame={t=self.frame.h-1,l=28, w=25},
+            label='Experience  ',
+            key='CUSTOM_CTRL_E',
+            options={
+                {label='No', value=false, pen=COLOR_WHITE},
+                {label='Yes', value=true, pen=COLOR_YELLOW},
+            },
+            view_id='toggle_experience',
+            on_change=function(option) self.display_experience = option end,
+        },
     }
 end
 
+-- Use render to set On/Off dynamically for each unit
+function SkillProgressOverlay:render(dc)
+    local unit = dfhack.gui.getSelectedUnit(true)
+
+    if unit then
+        self.subviews.toggle_progress:setOption(self.progress_bar)
+        self.subviews.toggle_experience:setOption(self.display_experience)
+    end
+
+    SkillProgressOverlay.super.render(self, dc)
+end
+
 function SkillProgressOverlay:onRenderFrame(dc, rect)
-    local margin = self.subviews.annotations.frame.l
+    local margin = self.subviews.annotations.frame.w
     local num_elems = self.frame.h // 3
     local max_elem = math.min(#view_sheets.unit_skill-1,
         view_sheets.scroll_position_unit_skill+num_elems-1)
@@ -70,72 +105,88 @@ function SkillProgressOverlay:onRenderFrame(dc, rect)
     for idx = view_sheets.scroll_position_unit_skill, max_elem do
         local skill = get_skill(idx)
         local rating = df.skill_rating.attrs[math.max(0, math.min(skill.rating, 19))]
-        local level = skill.rating
+        if self.display_experience then
+            if not self.progress_bar then
+                table.insert(annotations, NEWLINE)
+            end
+            local level_color = COLOR_GRAY
+            local level_chara = "\x1e"
 
-        local level_color = COLOR_GRAY
+            if skill.rating == 0 then
+                table.insert(annotations, "   ")
+            else
+                level_color = COLOR_WHITE
+                if skill.rating == df.skill_rating.Legendary then
+                    level_color = COLOR_CYAN
+                    level_chara = "\x0f"
+                end
 
-        if level == df.skill_rating.Legendary then
-            level_color = COLOR_WHITE
+                if skill.rating > df.skill_rating.Legendary then
+                    level_color = COLOR_LIGHTCYAN
+                    level_chara = "\x0f"
+                end
+
+                if skill.demotion_counter > 0 then
+                    level_color = COLOR_YELLOW
+                end
+
+                if skill.rating - skill.demotion_counter < 0 then
+                    level_color = COLOR_BROWN
+                    level_chara = "" -- level is going to be negative, so - takes its place
+                end
+
+                table.insert(annotations, { text=tostring(
+                        level_chara .. skill.rating - skill.demotion_counter
+                    ),
+                    width = 3,
+                    pen = level_color }
+                )
+            end
+
+            table.insert(annotations, "    ")
+
+            table.insert(annotations, { text=string.format(
+                    "%4d/%4d",
+                    skill.experience,
+                    rating.xp_threshold
+                ),
+                pen = level_color,
+                width = 9,
+                rjustify=true, }
+            )
         end
 
-        if level > df.skill_rating.Legendary then
-            level_color = COLOR_LIGHTCYAN
-        end
+        -- 3rd line (last)
 
-        if skill.demotion_counter > 0 then
-            level_color = COLOR_YELLOW
+        -- Progress Bar
+        if self.progress_bar then
+            table.insert(annotations, NEWLINE)
+            local percentage = skill.experience / rating.xp_threshold
+            local barstop = math.floor((margin * percentage) + 0.5)
+            for idx = 0, margin-1 do
+                local color = COLOR_LIGHTCYAN
+                local char = 219
+                -- start with the filled middle progress bar
+                local tex_idx = 1
+                -- at the beginning, use the left rounded corner
+                if idx == 0 then
+                    tex_idx = 0
+                end
+                -- at the beginning, use the right rounded corner
+                if idx == margin-1 then
+                    tex_idx = 2
+                end
+                if idx >= barstop then
+                    -- offset it to the hollow graphic
+                    tex_idx = tex_idx + 3
+                    color = COLOR_DARKGRAY
+                    char = 177
+                end
+                table.insert(annotations, { width = 1, tile={tile=df.global.init.load_bar_texpos[tex_idx], ch=char, fg=color}})
+            end
         end
-
-        if level - skill.demotion_counter <= 0 then
-            level_color = COLOR_BROWN
-        end
-
+        -- End!
         table.insert(annotations, NEWLINE)
-
-        table.insert(annotations, { text=string.format(
-                "L%d",
-                level - skill.demotion_counter
-            ),
-            width = 3,
-            pen = level_color }
-        )
-        table.insert(annotations, " -- ")
-
-        table.insert(annotations, { text=string.format(
-                "%4d/%4d",
-                skill.experience,
-                rating.xp_threshold
-            ),
-            pen = COLOR_GREEN,
-            width = 9,
-            rjustify=true, }
-        )
-
-        table.insert(annotations, NEWLINE)
-        local percentage = skill.experience / rating.xp_threshold
-        local barstop = math.floor((self.frame.w * percentage) + 0.5)
-        for idx = 0, self.frame.w-1 do
-            local color = COLOR_LIGHTCYAN
-            local char = 219
-            -- start with the filled middle progress bar
-            local tex_idx = 1
-            -- at the beginning, use the left rounded corner
-            if idx == 0 then
-                tex_idx = 0
-            end
-            -- at the beginning, use the right rounded corner
-            if idx == self.frame.w-1 then
-                tex_idx = 2
-            end
-            if idx >= barstop then
-                -- offset it to the hollow graphic
-                tex_idx = tex_idx + 3
-                color = COLOR_DARKGRAY
-                char = 177
-            end
-            table.insert(annotations, { width = 1, tile={tile=df.global.init.load_bar_texpos[tex_idx], ch=char, fg=color}})
-        end
-
         table.insert(annotations, NEWLINE)
     end
     self.subviews.annotations:setText(annotations)
