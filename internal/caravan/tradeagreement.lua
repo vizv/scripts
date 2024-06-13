b1f6aedb9d5138cb96264b6c7b6509d5324fe3c1
Myk Taylor <myk002@yahoo.com>
2024-06-12 19:21:43 -0700

    clean up implementation; add changelog

--- internal/caravan/tradeagreement.lua
+++ internal/caravan/tradeagreement.lua
@@ -4,81 +4,66 @@ local gui = require('gui')
 local overlay = require('plugins.overlay')
 local widgets = require('gui.widgets')
 
-TradeAgreementOverlay = defclass(TradeAgreementOverlay, overlay.OverlayWidget)
-TradeAgreementOverlay.ATTRS {
-    desc = 'Adds select all/none functionality when requesting trade agreement items.',
-    default_pos = { x = 45, y = -6 },
-    default_enabled = true,
-    viewscreens = 'dwarfmode/Diplomacy/Requests',
-    frame = { w = 40, h = 4 },
-    frame_style = gui.MEDIUM_FRAME,
-    frame_background = gui.CLEAR_PEN,
-}
 local diplomacy = df.global.game.main_interface.diplomacy
-local lasttab, lastresult = -1, nil
 
-local function tabsdef(pages, labelSingular, labelPlural, getCivResourceList, funcGetValue)
-    return {
-        pages = pages,
-        labelSingular = labelSingular,
-        labelPlural = labelPlural,
-        getCivResourceList = getCivResourceList,
-        funcGetValue = funcGetValue,
-    }
-end
+TradeAgreementOverlay = defclass(TradeAgreementOverlay, overlay.OverlayWidget)
+TradeAgreementOverlay.ATTRS{
+    desc='Adds quick toggles for groups of trade agreement items.',
+    default_pos={x=45, y=-6},
+    default_enabled=true,
+    viewscreens='dwarfmode/Diplomacy/Requests',
+    frame={w=40, h=4},
+    frame_style=gui.MEDIUM_FRAME,
+    frame_background=gui.CLEAR_PEN,
+}
 
-local Tabs = {}
-
-local function SquashMatIndexType(matList)
+local function transform_mat_list(matList)
     local list = {}
     for key, value in pairs(matList.mat_index) do
-        list[key] = { type = matList.mat_type[key], index = value }
+        list[key] = {type=matList.mat_type[key], index=value}
     end
     return list
 end
-local function DecodeMatInfoFromSquash(mat)
+
+local function decode_mat_list(mat)
     return dfhack.matinfo.decode(mat.type, mat.index).material.material_value
 end
 
-table.insert(Tabs, tabsdef({ 6, 7 }, "gem", "gems",
-    function(resources) return resources.gems end,
-    function(id) return dfhack.matinfo.decode(0, id).material.material_value end)
-)
-table.insert(Tabs, tabsdef({ 0 }, "leather", "leathers",
-    function(resources) return SquashMatIndexType(resources.organic.leather) end,
-    DecodeMatInfoFromSquash)
-)
-table.insert(Tabs, tabsdef({ 29 }, "meat", "meats",
-    function(resources) return SquashMatIndexType(resources.misc_mat.meat) end,
-    DecodeMatInfoFromSquash)
-)
-table.insert(Tabs, tabsdef({ 62 }, "parchment", "parchments",
-    function(resources) return SquashMatIndexType(resources.organic.parchment) end,
-    DecodeMatInfoFromSquash)
-)
+local select_by_value_tab = {
+    Leather={
+        get_mats=function(resources) return transform_mat_list(resources.organic.leather) end,
+        decode=decode_mat_list,
+    },
+    SmallCutGems={
+        get_mats=function(resources) return resources.gems end,
+        decode=function(id) return dfhack.matinfo.decode(0, id).material.material_value end,
+    },
+    Meat={
+        get_mats=function(resources) return transform_mat_list(resources.misc_mat.meat) end,
+        decode=decode_mat_list,
+    },
+    Parchment={
+        get_mats=function(resources) return transform_mat_list(resources.organic.parchment) end,
+        decode=decode_mat_list,
+    },
+}
+select_by_value_tab.LargeCutGems = select_by_value_tab.SmallCutGems
 
-local function GetCurrentTabId()
-    return diplomacy.taking_requests_tablist[diplomacy.taking_requests_selected_tab];
+local function get_cur_tab_category()
+    return diplomacy.taking_requests_tablist[diplomacy.taking_requests_selected_tab]
 end
 
-local function GetTab()
-    local curtab = GetCurrentTabId()
-    if (curtab == lasttab) then return lastresult end
-    lasttab = curtab
-    for _, tab in ipairs(Tabs) do
-        for _, page in ipairs(tab.pages) do
-            if page == curtab then
-                lastresult = tab
-                return tab
-            end
-        end
-    end
-    lastresult = nil
+local function get_select_by_value_tab(category)
+    category = category or get_cur_tab_category()
+    return select_by_value_tab[df.entity_sell_category[category]]
+end
+
+local function get_cur_priority_list()
+    return diplomacy.environment.dipev.sell_requests.priority[get_cur_tab_category()]
 end
 
 local function diplomacy_toggle_cat()
-    local priority_idx = GetCurrentTabId()
-    local priority = diplomacy.environment.dipev.sell_requests.priority[priority_idx]
+    local priority = get_cur_priority_list()
     if #priority == 0 then return end
     local target_val = priority[0] == 0 and 4 or 0
     for i in ipairs(priority) do
@@ -86,71 +71,71 @@ local function diplomacy_toggle_cat()
     end
 end
 
-local function OrderGemWithMinValue(matPrices, val)
-    local priority_idx = GetCurrentTabId()
-
-    local priority = diplomacy.environment.dipev.sell_requests.priority[priority_idx]
+local function select_by_value(prices, val)
+    local priority = get_cur_priority_list()
     for i in ipairs(priority) do
-        if matPrices[i] == val then
+        if prices[i] == val then
             priority[i] = 4
         end
     end
 end
 
-local function GetCivMatPrices(tabSelected)
-    local resource = tabSelected.getCivResourceList(df.historical_entity.find(diplomacy.actor.civ_id).resources)
-    local matPrices = {}
+function TradeAgreementOverlay:init()
+    self:addviews{
+        widgets.HotkeyLabel{
+            frame={t=0, l=0},
+            label='Select all/none',
+            key='CUSTOM_CTRL_A',
+            on_activate=diplomacy_toggle_cat,
+        },
+    }
+    self:addviews{
+        widgets.HotkeyLabel{
+            frame={t=1, l=0},
+            label='Select materials by value',
+            key='CUSTOM_CTRL_M',
+            on_activate=self:callback('select_by_value'),
+            enabled=get_select_by_value_tab,
+        },
+    }
+end
+
+local function get_prices(tab)
+    local resource = tab.get_mats(df.historical_entity.find(diplomacy.actor.civ_id).resources)
+    local prices = {}
     local matValuesUnique = {}
     local filter = {}
     for civid, matid in pairs(resource) do
-        local matPrice = tabSelected.funcGetValue(matid)
-        matPrices[civid] = matPrice
-        if not filter[matPrice] then
-            local val = { value = matPrice, count = 1 }
-            filter[matPrice] = val
+        local price = tab.decode(matid)
+        prices[civid] = price
+        if not filter[price] then
+            local val = {value=price, count=1}
+            filter[price] = val
             table.insert(matValuesUnique, val)
         else
-            filter[matPrice].count = filter[matPrice].count + 1
+            filter[price].count = filter[price].count + 1
         end
     end
     table.sort(matValuesUnique, function(a, b) return a.value < b.value end)
-    return matPrices, matValuesUnique
+    return prices, matValuesUnique
 end
 
-local function ValueSelector()
-    local currTab = GetTab()
-    local matPrices, matValuesUnique = GetCivMatPrices(currTab)
+function TradeAgreementOverlay:select_by_value()
+    local cat = get_cur_tab_category()
+    local cur_tab = get_select_by_value_tab(cat)
+
+    local resource_name = df.entity_sell_category[cat]
+    if resource_name:endswith('Gems') then resource_name = 'Gem' end
+    local prices, matValuesUnique = get_prices(cur_tab)
     local list = {}
     for index, value in ipairs(matValuesUnique) do
-        list[index] = tostring(value.value) ..
-            " - " .. tostring(value.count) .. " " .. (value.count == 1 and currTab.labelSingular or currTab.labelPlural)
+        list[index] = ('%4d%s (%d type%s of %s)'):format(
+            value.value, string.char(15), value.count, value.count == 1 and '' or 's', resource_name:lower())
     end
     dlg.showListPrompt(
         "Select materials with base value", "",
         COLOR_WHITE,
         list,
-        function(id, choice)
-            OrderGemWithMinValue(matPrices, matValuesUnique[id].value)
-        end
+        function(id) select_by_value(prices, matValuesUnique[id].value) end
     )
-end
-
-function TradeAgreementOverlay:init()
-    self:addviews {
-        widgets.HotkeyLabel {
-            frame = { t = 0, l = 0 },
-            label = 'Select all/none',
-            key = 'CUSTOM_CTRL_A',
-            on_activate = diplomacy_toggle_cat,
-        },
-    }
-    self:addviews {
-        widgets.HotkeyLabel {
-            frame = { t = 1, l = 0 },
-            label = 'Select materials with value',
-            key = 'CUSTOM_CTRL_M',
-            on_activate = ValueSelector,
-            visible = function() return GetTab() ~= nil end,
-        },
-    }
 end
