3c96ab945ee7bfe21f5b3aabb43e5ca74aab0716
Myk Taylor <myk002@yahoo.com>
2024-06-21 17:57:20 -0700

    add depot pathability mode

--- gui/pathable.lua
+++ gui/pathable.lua
@@ -6,15 +6,11 @@ local plugin = require('plugins.pathable')
 local widgets = require('gui.widgets')
 
 -- ------------------------------
--- Pathable
+-- FollowMousePage
 
-Pathable = defclass(Pathable, widgets.Window)
-Pathable.ATTRS {
-    frame_title='Pathability Viewer',
-    frame={t=18, r=2, w=30, h=9},
-}
+FollowMousePage = defclass(FollowMousePage, widgets.Panel)
 
-function Pathable:init()
+function FollowMousePage:init()
     self:addviews{
         widgets.ToggleHotkeyLabel{
             view_id='draw',
@@ -52,7 +48,7 @@ function Pathable:init()
     }
 end
 
-function Pathable:onRenderBody()
+function FollowMousePage:onRenderBody()
     local target = self.subviews.lock:getOptionValue() and
             self.saved_target or dfhack.gui.getMousePos()
     self.saved_target = target
@@ -75,6 +71,114 @@ function Pathable:onRenderBody()
 end
 
 -- ------------------------------
+-- DepotPage
+
+DepotPage = defclass(DepotPage, widgets.Panel)
+DepotPage.ATTRS {
+    force_pause=true,
+}
+
+function DepotPage:init()
+    if dfhack.world.isAdventureMode() then
+        self:addviews{
+            widgets.WrappedLabel{
+                text_to_wrap='Not available in adventure mode.',
+                text_pen=COLOR_YELLOW,
+            },
+        }
+        return
+    end
+
+    self.animals = false
+    self.wagons = false
+
+    local TD = df.global.world.buildings.other.TRADE_DEPOT
+    local depot_idx = 0
+
+    self:addviews{
+        widgets.Label{
+            frame={t=0, l=0},
+            text='Depot is reachable by:',
+        },
+        widgets.Label{
+            frame={t=1, l=2},
+            text={
+                'Pack animals:',
+                {gap=1, text=function() return self.animals and 'Yes' or 'No' end,
+                 pen=function() return self.animals and COLOR_GREEN or COLOR_RED end},
+            },
+        },
+        widgets.Label{
+            frame={t=2, l=8},
+            text={
+                'Wagons:',
+                {gap=1, text=function() return self.wagons and 'Yes' or 'No' end,
+                 pen=function() return self.wagons and COLOR_GREEN or COLOR_RED end},
+            },
+        },
+        widgets.HotkeyLabel{
+            frame={b=0, l=0},
+            key='CUSTOM_CTRL_D',
+            label='Zoom to depot',
+            on_activate=function()
+                depot_idx = depot_idx + 1
+                if depot_idx >= #TD then depot_idx = 0 end
+                local bld = TD[depot_idx]
+                dfhack.gui.revealInDwarfmodeMap(xyz2pos(bld.centerx, bld.centery, bld.z), true, true)
+            end,
+            enabled=#TD > 0,
+        },
+    }
+end
+
+function DepotPage:on_show()
+    self.animals = plugin.getDepotAccessibleByAnimals()
+    self.wagons = plugin.getDepotAccessibleByWagons(true)
+end
+
+function DepotPage:onRenderBody()
+    plugin.paintScreenDepotAccess()
+end
+
+-- ------------------------------
+-- Pathable
+
+Pathable = defclass(Pathable, widgets.Window)
+Pathable.ATTRS {
+    frame_title='Pathability Viewer',
+    frame={t=18, r=2, w=30, h=12},
+}
+
+function Pathable:init()
+    self:addviews{
+        widgets.TabBar{
+            frame={t=0, l=0},
+            labels={
+                'Follow mouse',
+                'Depot',
+            },
+            on_select=function(idx)
+                self.subviews.pages:setSelected(idx)
+                local _, page = self.subviews.pages:getSelected()
+                self.parent_view.force_pause = page.force_pause
+                if page.on_show then
+                    page:on_show()
+                end
+            end,
+            get_cur_page=function() return self.subviews.pages:getSelected() end,
+        },
+        widgets.Pages{
+            view_id='pages',
+            frame={t=3, l=0, b=0, r=0},
+            subviews={
+                FollowMousePage{},
+                DepotPage{},
+            },
+        },
+    }
+end
+
+-- ------------------------------
 -- PathableScreen
 
 PathableScreen = defclass(PathableScreen, gui.ZScreen)
@@ -94,6 +198,9 @@ end
 if dfhack_flags.module then
     return
 end
+
+-- ------------------------------
+-- CLI
 
 if not dfhack.isMapLoaded() then
     qerror('gui/pathable requires a map to be loaded')
