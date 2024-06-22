15788a461c33490f5502a4ee7d339a3b3c7c1a09
Alex Noir <Varsash@gmail.com>
2024-06-22 16:27:02 +0300

    Update the block only if clearing smoke was a success

--- clear-smoke.lua
+++ clear-smoke.lua
@@ -1,23 +1,27 @@
-local flowsToDelete = {}
+local flows_to_delete = {}
 
 function clearSmoke(flows)
+    local success
     for i = #flows - 1, 0, -1 do
         local flow = flows[i]
         if flow.type == df.flow_type.Smoke then
             flows:erase(i)
-            flowsToDelete[flow] = true
+            flows_to_delete[flow] = true
+            success = true
         end
     end
+    return success
 end
 
 clearSmoke(df.global.flows)
 
-for _, block in pairs(df.global.world.map.map_blocks) do
-    clearSmoke(block.flows)
-    dfhack.maps.enableBlockUpdates(block, true)
+for _,block in pairs(df.global.world.map.map_blocks) do
+    if clearSmoke(block.flows) then
+        dfhack.maps.enableBlockUpdates(block, true)
+    end
 end
 
-for flow,_ in pairs(flowsToDelete) do
+for flow,_ in pairs(flows_to_delete) do
     if flow then
         flow:delete()
     end
