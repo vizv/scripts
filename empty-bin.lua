3fe2c2581a6dc9865b3480ea9a5542f82cc7e7a3
Alex Noir <Varsash@gmail.com>
2024-06-24 15:16:22 +0300

    do not use mouse position to determine emptying - instead, entirely rely on the viewsheet Add support for emptying buildings and items dumped on the ground as well

--- empty-bin.lua
+++ empty-bin.lua
@@ -12,14 +12,18 @@ Empties the contents of the selected bin onto the floor.
 
 ]====]
 
+local function moveItem(item, to_pos)
+    print('  ' .. dfhack.items.getDescription(item, 0))
+    dfhack.items.moveToGround(item, to_pos)
+end
+
 local function emptyContainer(container)
     local items = dfhack.items.getContainedItems(container)
 
     if #items > 0 then
         print('Emptying ' .. dfhack.items.getDescription(container, 0))
-        for _, item in pairs(items) do
-            print('  ' .. dfhack.items.getDescription(item, 0))
-            dfhack.items.moveToGround(item, xyz2pos(dfhack.items.getPosition(container)))
+        for _, item in ipairs(items) do
+            moveItem(item, xyz2pos(dfhack.items.getPosition(container)))
         end
     end
 end
@@ -33,19 +37,34 @@ local function emptyPos(pos)
     end
 end
 
+local viewsheets = df.global.game.main_interface.view_sheets
 
 local stockpile = dfhack.gui.getSelectedStockpile(true)
-local mouse_pos = dfhack.gui.getMousePos()
-local selected = dfhack.gui.getSelectedItem(true)
+local selectedItem = dfhack.gui.getSelectedItem(true)
+local selectedBuilding = dfhack.gui.getSelectedBuilding(true)
 if stockpile then
     local contents = dfhack.buildings.getStockpileContents(stockpile)
     for _, container in ipairs(contents) do
         emptyContainer(container)
     end
-elseif selected then
-    emptyContainer(selected)
-elseif mouse_pos then
-    emptyPos(mouse_pos)
+elseif selectedItem then
+    emptyContainer(selectedItem)
+elseif selectedBuilding then
+    if not df.building_actual:is_instance(selectedBuilding) then
+        qerror("This type of building does not contain any items!")
+    end
+    local containedItems = selectedBuilding.contained_items
+    for i=#containedItems-1,0,-1 do
+        contained = containedItems[i]
+        if contained.use_mode == df.building_item_role_type.TEMP then
+            moveItem(contained.item, xyz2pos(selectedBuilding.centerx, selectedBuilding.centery, selectedBuilding.z))
+        end
+    end
+elseif viewsheets.open then
+    for _, item_id in ipairs(viewsheets.viewing_itid) do
+        local item = df.item.find(item_id)
+        emptyContainer(item)
+    end
 else
-    qerror("No item selected")
+    qerror("No valid target found")
 end
