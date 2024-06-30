b1bbdeace15b68afcd79eb9c2317995e62cbe937
Alex Noir <Varsash@gmail.com>
2024-06-30 19:44:05 +0300

    Adjust pets that are incorrectly in your party_pets list but belong to a different owner

--- fix/gifted-pets.lua
+++ fix/gifted-pets.lua
@@ -20,7 +20,7 @@ end
 
 local function fixMissingCompanionPets(nemesis)
     local party = df.global.adventure.interactions
-    for i, nem_id in ipairs(nemesis.companions) do
+    for _, nem_id in ipairs(nemesis.companions) do
         local pet_record = df.nemesis_record.find(nem_id)
         if findPetOwnerOf(pet_record) == nemesis and not findInParty(party.party_pets, pet_record.figure.id) then
             print("inserting pet "..pet_record.id.." of companion "..nemesis.id.." into the party!")
@@ -40,7 +40,7 @@ local function fixGiftedPet(pet_record, pet_owner_record)
     
     -- associate the site links
     local site_links = pet_owner_record.figure.site_links
-    for u, link in ipairs(site_links) do
+    for _, link in ipairs(site_links) do
         if link._type == df.histfig_site_link_home_site_abstract_buildingst or
         link._type == df.histfig_site_link_home_site_realization_buildingst or
         link._type == df.histfig_site_link_lairst or
@@ -54,10 +54,25 @@ local function fixGiftedPet(pet_record, pet_owner_record)
     end
 end
 
+local function adjustPetOwner(pet_owner_record, pet_record)
+    local party = df.global.adventure.interactions
+    local owner_in_party = findInParty(party.party_core_members, pet_owner_record.figure.id) or findInParty(party.party_extra_members, pet_owner_record.figure.id)
+    if not owner_in_party then
+        print("the pet's owner is NOT a companion. We need to associate the pet with the owner correctly so the pet's unit is not inaccessible.")
+        fixGiftedPet(pet_record, pet_owner_record)
+        utils.erase_sorted(party.party_pets, pet_record.figure.id)
+        return true
+    else
+        print("the pet's owner is a companion! Assigning their pets to our follower pets.")
+        party.party_pets:insert('#', pet_record.figure.id)
+        return false
+    end
+end
+
 local function fixGiftedPets(nemesis)
     local queueCompanionErase = {}
     local party = df.global.adventure.interactions
-    for i, nem_id in ipairs(nemesis.companions) do
+    for _, nem_id in ipairs(nemesis.companions) do
         local companion_record = df.nemesis_record.find(nem_id)
         fixMissingCompanionPets(companion_record)
 
@@ -66,19 +81,23 @@ local function fixGiftedPets(nemesis)
             local pet_owner_record = findPetOwnerOf(companion_record)
             if pet_owner_record then
                 print("pet owner confirmed! Identified nemesis "..pet_owner_record.id.." as owner.")
-                local party = party
-                local owner_in_party = findInParty(party.party_core_members, pet_owner_record.figure.id) or findInParty(party.party_extra_members, pet_owner_record.figure.id)
-                if not owner_in_party then
-                    print("the pet's owner is NOT a companion. We need to associate the pet with the owner correctly so the pet's unit is not inaccessible.")
-                    fixGiftedPet(companion_record, pet_owner_record)
+                if adjustPetOwner(pet_owner_record, pet_record) then
                     queueCompanionErase[companion_record.id] = true
-                else
-                    print("the pet's owner is a companion! Assigning their pets to our follower pets.")
-                    party.party_pets:insert('#', companion_record.figure.id)
                 end
             end
         end
     end
+
+    for _, histfig_id in ipairs(party.party_pets) do
+        local pet_figure = df.historical_figure.find(histfig_id)
+        local pet_record = df.nemesis_record.find(pet_figure.nemesis_id)
+        local pet_owner_record = findPetOwnerOf(pet_record)
+        if pet_owner_record then
+            print("pet owner confirmed! Identified nemesis "..pet_owner_record.id.." as owner.")
+            adjustPetOwner(pet_owner_record, pet_record)
+        end
+    end
+
     for index = #nemesis.companions-1, 0, -1 do
         if queueCompanionErase[nemesis.companions[index]] then
             print("erasing bugged pet "..nemesis.companions[index].." from "..nemesis.id.."'s companions list")
